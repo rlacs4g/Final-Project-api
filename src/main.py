0840fef71544caca71cdd65c20be5a45243e9e74
rlacs4g <lacson.rafael@gmail.com>
2021-02-19 23:31:49 +0000

    init commit

--- src/main.py
+++ src/main.py
@@ -10,15 +10,23 @@ from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User
 #from models import Person
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+from werkzeug.security import generate_password_hash, check_password_hash
+import datetime
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
+app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
+jwt = JWTManager(app)
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -38,6 +46,83 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@app.route('/register', methods=['POST'])
+def register():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    fname = request.json.get("fname", None)
+    lname = request.json.get("lname", None)
+    tos = request.json.get("tos", None) 
+
+    if not email:
+        return jsonify({"msg": "Email is required"}), 400
+    if not password:
+        return jsonify({"msg": "Password is required"}), 400
+    if not fname:
+        return jsonify({"msg": "First name is required"}), 400
+    if not lname:
+        return jsonify({"msg": "Last name is required"}), 400
+    if not tos:
+        return jsonify({"msg": "tos is required"}), 400
+
+    user = User.query.filter_by(email=email).first()
+    if user is not None:
+        return jsonify({"msg": "Email address already exists"}), 400
+
+    user = User(email=email, password=generate_password_hash(password), firstname=fname, lastname=lname, tos=tos, is_active=True)
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify({"msg": "User successfully registered"}),200
+
+# Provide a method to create access tokens. The create_jwt()
+# function is used to actually generate the token
+@app.route('/login', methods=['POST'])
+def login():
+    # make sure request is a json
+    if not request.is_json:
+        return jsonify({"msg": "Missing JSON in request"}), 400
+
+    # get email and password from request
+    email = request.json.get('email', None)
+    password = request.json.get('password', None)
+
+    # if params are empty, return a 400
+    if not email:
+        return jsonify({"msg": "Missing email parameter"}), 400
+    if not password:
+        return jsonify({"msg": "Missing password parameter"}), 400
+
+    # try to find user
+    try:
+        # query user
+        user = User.query.filter_by(email=email).first()
+        # test validate method. 
+        if user.validate(password):
+            # if user is validated (password is correct), return the token
+            expires = datetime.timedelta(days=7)
+            response_msg = {
+                "user": user.serialize(),
+                'token': create_access_token(identity=email,expires_delta=expires),
+                # 'token': create_access_token(identity=email),
+                "expires_at": expires.total_seconds()*1000 
+            }
+            status_code = 200
+        else:
+            # otherwise, raise an exception so that they check their email and password
+            raise Exception('Failed to login. Check your email and password.')
+    # catch the specific exception and store in var
+    except Exception as e:
+        # format a json respons with the exception
+        response_msg = {
+            'msg': str(e),
+            'status': 401
+        }
+        status_code = 401
+    
+    # general return in json with status
+    return jsonify(response_msg), status_code
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,19 +1,48 @@
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(50), nullable=False)
+    lastname = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(256), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    tos = db.Column(db.Boolean(), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    diary = db.relationship('Food', backref = 'user')
+    # Diary (relationship 1 to n -> Food)
 
-    def __repr__(self):
+
+    def repr(self):
         return '<User %r>' % self.username
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "first_name": self.firstname,
+            "last_name": self.lastname
+
             # do not serialize the password, its a security breach
         }
+
+    def validate(self,password):
+        if not check_password_hash(self.password, password):
+            return False
+
+        return True
+
+class Food(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    calories = db.Column(db.String(50), nullable=False)
+    serving_size = db.Column(db.String(120), unique=True, nullable=False)
+    quantity = db.Column(db.String(80), unique=False, nullable=False)
+    date = db.Column(db.String(50), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    time_of_day = db.Column(db.Enum('morning','afternoon','night'), nullable=False, server_default='morning')
+
+# Date (datetime stamp)
+# Time of day (ENUM morning | afternoon | evening)
